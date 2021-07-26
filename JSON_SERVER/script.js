0475886943023abadcc0063c1d2a7f864816a183
Felipe Vieira <87919419+UFOsHuygens@users.noreply.github.com>
2021-07-26 14:54:50 -0300

    Delete JSON_SERVER directory

--- JSON_SERVER/script.js
+++
@@ -1,9 +0,0 @@
-var h1 = document.querySelector("h1");
-
-fetch("http://localhost:3000/solarsystem_db").then(response => response.json())
-.then(json => {json.map(item => {
-    h1.innerHTML += `${item.planet}<br>`
-})})
-
-// npm install -g json-server
-// json-server --watch db.json
