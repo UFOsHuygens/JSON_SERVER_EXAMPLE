026c741ec542f2e8f4489b21f949828d0202d458
Felipe Vieira <87919419+UFOsHuygens@users.noreply.github.com>
2021-07-26 15:02:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,3 @@ fetch("http://localhost:3000/solarsystem_db").then(response => response.json())
 .then(json => {json.map(item => {
     h1.innerHTML += `${item.planet}<br>`
 })})
-
-// npm install -g json-server
-// json-server --watch db.json
